b79f9ceb6225d0982cdaa4ef58117957f283f7d9
Sukka <isukkaw@gmail.com>
2019-08-15 09:52:04 +0800

    Add a test with meta_generator comment out

--- perf-test.sh
+++ perf-test.sh
@@ -284,6 +284,70 @@ cat perf.log | grep 'generated in'
 
 echo '-------------------------------------'
 echo '               Test H'
+echo ' Disable meta_generator'
+echo '-------------------------------------'
+echo ' - fragment_fache: on'
+echo ' - hexo built in highlight.js'
+echo '    - enable: false'
+echo '    - line_number: false'
+echo '    - auto_detect: false'
+echo '    - tab_replace: false'
+echo ' - suka theme prism highlight: off'
+echo ' - suka theme local-search: off'
+echo '-------------------------------------'
+
+../bin/yq w -i themes/suka/_config.yml fragment_cache true
+
+../bin/yq w -i _config.yml highlight.enable false
+../bin/yq w -i _config.yml highlight.line_number false
+../bin/yq w -i _config.yml highlight.auto_detect false
+../bin/yq w -i _config.yml highlight.tab_replace false
+
+../bin/yq w -i _config.yml suka_theme.prism.enable false
+../bin/yq w -i _config.yml suka_theme.prism.line_number false
+
+../bin/yq w -i _config.yml suka_theme.search.enable false
+
+../bin/yq w -i _config.yml meta_generator false
+
+echo -n 'Round 1: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo -n 'Round 2: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo '-------------------------------------'
+echo '               Test I'
+echo ' Comment meta_generator'
+echo '-------------------------------------'
+echo ' - fragment_fache: on'
+echo ' - hexo built in highlight.js'
+echo '    - enable: false'
+echo '    - line_number: false'
+echo '    - auto_detect: false'
+echo '    - tab_replace: false'
+echo ' - suka theme prism highlight: off'
+echo ' - suka theme local-search: off'
+echo '-------------------------------------'
+
+sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g"  ./node_modules/hexo/lib/plugins/filter/index.js
+
+echo -n 'Round 1: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo -n 'Round 2: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo '-------------------------------------'
+echo '               Test J'
 echo ' Test hexo-renderer-extended-ejs'
 echo '-------------------------------------'
 echo ' - fragment_fache: on'
@@ -321,41 +385,3 @@ npm run clean > /dev/null
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
 
-echo '* Test performance ... '
-echo '-------------------------------------'
-echo '               Test I'
-echo ' Disable meta_generator'
-echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
-echo '-------------------------------------'
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
-
-../bin/yq w -i _config.yml highlight.enable false
-../bin/yq w -i _config.yml highlight.line_number false
-../bin/yq w -i _config.yml highlight.auto_detect false
-../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
-../bin/yq w -i _config.yml meta_generator false
-
-echo -n 'Round 1: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo -n 'Round 2: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
