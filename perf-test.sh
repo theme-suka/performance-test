4782963b3e859419777abf2e2cdb3258de2f89a6
SukkaW <isukkaw@gmail.com>
2019-08-15 11:02:20 +0800

    Add a test to disable external_link

--- perf-test.sh
+++ perf-test.sh
@@ -59,6 +59,43 @@ npm run clean > /dev/null
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
 
+echo '-------------------------------------'
+echo '               Test A'
+echo ' Disable external_link'
+echo '-------------------------------------'
+echo ' - fragment_fache: on'
+echo ' - hexo built in highlight.js'
+echo '    - enable: false'
+echo '    - line_number: false'
+echo '    - auto_detect: false'
+echo '    - tab_replace: false'
+echo ' - suka theme prism highlight: off'
+echo ' - suka theme local-search: off'
+echo '-------------------------------------'
+
+../bin/yq w -i themes/suka/_config.yml fragment_cache true
+
+../bin/yq w -i _config.yml highlight.enable false
+../bin/yq w -i _config.yml highlight.line_number false
+../bin/yq w -i _config.yml highlight.auto_detect false
+../bin/yq w -i _config.yml highlight.tab_replace false
+
+../bin/yq w -i _config.yml suka_theme.prism.enable false
+../bin/yq w -i _config.yml suka_theme.prism.line_number false
+
+../bin/yq w -i _config.yml suka_theme.search.enable false
+
+../bin/yq w -i _config.yml external_link false
+
+echo -n 'Round 1: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo -n 'Round 2: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
 
 echo '-------------------------------------'
 echo '               Test B'
