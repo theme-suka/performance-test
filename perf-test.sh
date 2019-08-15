950c4008bcd6eb1ccf914b440696966cc051342c
SukkaW <isukkaw@gmail.com>
2019-08-15 21:52:02 +0800

    Bring up a test related to  mass files

--- perf-test.sh
+++ perf-test.sh
@@ -25,48 +25,11 @@ echo -n 'Done!
 npm uninstall hexo --save
 npm install git+https://github.com/hexojs/hexo.git
 
+echo '-------------------------------------'
 echo '* Test performance ... '
 echo '-------------------------------------'
 echo '               Test A'
-echo ' Best performance'
-echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
-echo '-------------------------------------'
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
-
-../bin/yq w -i _config.yml highlight.enable false
-../bin/yq w -i _config.yml highlight.line_number false
-../bin/yq w -i _config.yml highlight.auto_detect false
-../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
-../bin/yq w -i _config.yml meta_generator true
-
-echo -n 'Round 1: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo -n 'Round 2: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo '-------------------------------------'
-echo '               Test B'
-echo ' Disable meta_generator'
+echo ' Baseline'
 echo '-------------------------------------'
 echo ' - fragment_fache: on'
 echo ' - hexo built in highlight.js'
@@ -91,6 +54,42 @@ echo '-------------------------------------'
 ../bin/yq w -i _config.yml suka_theme.search.enable false
 
 ../bin/yq w -i _config.yml meta_generator false
+
+echo -n 'Round 1: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo -n 'Round 2: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo '-------------------------------------'
+echo '               Test B'
+echo ' Remove useless files'
+echo '-------------------------------------'
+echo ' - fragment_fache: on'
+echo ' - hexo built in highlight.js'
+echo '    - enable: false'
+echo '    - line_number: false'
+echo '    - auto_detect: false'
+echo '    - tab_replace: false'
+echo ' - suka theme prism highlight: off'
+echo ' - suka theme local-search: off'
+echo '-------------------------------------'
+
+# Run Clean up
+echo -n '
+* Run clean up ... '
+
+rm -rf node_modules/**/test/**/*
+rm -rf node_modules/**/*.test
+rm -rf themes/suka/node_modules/**/*.test
+rm -rf node_modules/**/docs/**/*
+
+echo -n 'Done!
+'
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
