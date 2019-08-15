5d7aa90f6df2f5f1cd92bb3580824c68475387e7
SukkaW <isukkaw@gmail.com>
2019-08-15 22:20:24 +0800

    Remove useless test

--- perf-test.sh
+++ perf-test.sh
@@ -86,115 +86,12 @@ echo -n '
 rm -rf node_modules/**/test/**/*
 rm -rf node_modules/**/*.test
 rm -rf themes/suka/node_modules/**/*.test
+rm -rf themes/suka/node_modules/**/*.md
 rm -rf node_modules/**/docs/**/*
+rm -rf node_modules/**/LICENSE*
 
 echo -n 'Done!
 '
-
-echo -n 'Round 1: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo -n 'Round 2: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo '-------------------------------------'
-echo '               Test C'
-echo ' Comment meta_generator'
-echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
-echo '-------------------------------------'
-
-sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g"  ./node_modules/hexo/lib/plugins/filter/index.js
-
-echo -n 'Round 1: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo -n 'Round 2: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-npm uninstall hexo --save
-npm install git+https://github.com/sukkaw/hexo.git#meta-generator-config
-
-echo '-------------------------------------'
-echo '               Test D'
-echo ' Best performance'
-echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
-echo '-------------------------------------'
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
-
-../bin/yq w -i _config.yml highlight.enable false
-../bin/yq w -i _config.yml highlight.line_number false
-../bin/yq w -i _config.yml highlight.auto_detect false
-../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
-../bin/yq w -i _config.yml meta_generator true
-
-echo -n 'Round 1: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo -n 'Round 2: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo '-------------------------------------'
-echo '               Test E'
-echo ' Disable meta_generator'
-echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
-echo '-------------------------------------'
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
-
-../bin/yq w -i _config.yml highlight.enable false
-../bin/yq w -i _config.yml highlight.line_number false
-../bin/yq w -i _config.yml highlight.auto_detect false
-../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
-../bin/yq w -i _config.yml meta_generator false
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
