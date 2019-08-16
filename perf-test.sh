127e75d557e285610340481e40a7b5d2a96adfcd
SukkaW <isukkaw@gmail.com>
2019-08-16 10:40:25 +0800

    Bring up test for lazy-moment

--- perf-test.sh
+++ perf-test.sh
@@ -54,45 +54,6 @@ echo '-------------------------------------'
 ../bin/yq w -i _config.yml suka_theme.search.enable false
 
 ../bin/yq w -i _config.yml meta_generator false
-../bin/yq w -i _config.yml external_link true
-
-echo -n 'Round 1: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo -n 'Round 2: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo '-------------------------------------'
-echo '               Test B'
-echo ' Disable external link'
-echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
-echo '-------------------------------------'
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
-
-../bin/yq w -i _config.yml highlight.enable false
-../bin/yq w -i _config.yml highlight.line_number false
-../bin/yq w -i _config.yml highlight.auto_detect false
-../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
-../bin/yq w -i _config.yml meta_generator false
 ../bin/yq w -i _config.yml external_link false
 
 echo -n 'Round 1: '
@@ -104,3 +65,32 @@ echo -n 'Round 2: '
 npm run clean > /dev/null
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
+
+npm uninstall hexo --save
+npm install git+https://github.com/sukkaw/hexo.git#lazy-moment
+
+echo '-------------------------------------'
+echo '               Test B'
+echo ' Use https://github.com/sukkaw/hexo.git#lazy-moment'
+echo '-------------------------------------'
+echo ' - fragment_fache: on'
+echo ' - hexo built in highlight.js'
+echo '    - enable: false'
+echo '    - line_number: false'
+echo '    - auto_detect: false'
+echo '    - tab_replace: false'
+echo ' - suka theme prism highlight: off'
+echo ' - suka theme local-search: off'
+echo '-------------------------------------'
+
+echo -n 'Round 1: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo -n 'Round 2: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo '-------------------------------------'
