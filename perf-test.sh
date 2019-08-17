a1fbe7cfa89ab74f7c1a363c12ab4e46ca9ad4f0
SukkaW <isukkaw@gmail.com>
2019-08-17 16:49:15 +0800

    Bring up a test for Hexo Pull #3655

--- perf-test.sh
+++ perf-test.sh
@@ -19,14 +19,11 @@ rm -rf source/assets
 echo -n 'Done!
 '
 
-npm uninstall hexo --save
-npm install git+https://github.com/hexojs/hexo.git
-
 echo '-------------------------------------'
 echo '* Test performance ... '
 echo '-------------------------------------'
 echo '               Test A'
-echo ' Baseline'
+echo ' Baseline with meta_generator comment out'
 echo '-------------------------------------'
 echo ' - hexo built in highlight.js'
 echo '    - enable: false'
@@ -45,6 +42,8 @@ echo '-------------------------------------'
 
 ../bin/yq w -i _config.yml theme landscape
 
+sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
+
 echo -n 'Round 1: '
 npm run clean > /dev/null
 npm run generate > perf.log
@@ -56,11 +55,11 @@ npm run generate > perf.log
 cat perf.log | grep 'generated in'
 
 npm uninstall hexo --save
-npm install git+https://github.com/curbengh/hexo.git#metagen-cheerio
+npm install git+https://github.com/ppoffice/hexo.git
 
 echo '-------------------------------------'
 echo '               Test B'
-echo ' Use https://github.com/curbengh/hexo.git#metagen-cheerio'
+echo ' Use https://github.com/ppoffice/hexo#master'
 echo '-------------------------------------'
 echo ' - hexo built in highlight.js'
 echo '    - enable: false'
@@ -74,10 +73,12 @@ echo '-------------------------------------'
 ../bin/yq w -i _config.yml highlight.auto_detect false
 ../bin/yq w -i _config.yml highlight.tab_replace false
 
-../bin/yq w -i _config.yml meta_generator true
+../bin/yq w -i _config.yml meta_generator false
 ../bin/yq w -i _config.yml external_link false
 
 ../bin/yq w -i _config.yml theme landscape
+
+sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
