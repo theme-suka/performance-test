6c1ce065ca7196fa89032f34773d0c497da91f16
SukkaW <isukkaw@gmail.com>
2019-08-19 16:10:47 +0800

    Test with regex, dom-parser & cheerio

--- perf-test.sh
+++ perf-test.sh
@@ -115,3 +115,50 @@ echo -n 'Round 2: '
 npm run clean > /dev/null
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
+
+
+npm uninstall hexo --save
+npm install git+https://github.com/curbengh/hexo.git#og-domparser
+
+echo '-------------------------------------'
+echo '               Test C'
+echo ' Use curbengh:og-domparser'
+echo '-------------------------------------'
+echo ' - fragment_fache: on'
+echo ' - hexo built in highlight.js'
+echo '    - enable: false'
+echo '    - line_number: false'
+echo '    - auto_detect: false'
+echo '    - tab_replace: false'
+echo ' - suka theme prism highlight: off'
+echo ' - suka theme local-search: off'
+echo '-------------------------------------'
+
+sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
+
+../bin/yq w -i themes/suka/_config.yml fragment_cache true
+
+../bin/yq w -i _config.yml highlight.enable false
+../bin/yq w -i _config.yml highlight.line_number false
+../bin/yq w -i _config.yml highlight.auto_detect false
+../bin/yq w -i _config.yml highlight.tab_replace false
+
+../bin/yq w -i _config.yml suka_theme.prism.enable false
+../bin/yq w -i _config.yml suka_theme.prism.line_number false
+
+../bin/yq w -i _config.yml suka_theme.search.enable false
+
+../bin/yq w -i _config.yml meta_generator false
+../bin/yq w -i _config.yml external_link false
+
+../bin/yq w -i themes/suka/_config.yml toc.enable false
+
+echo -n 'Round 1: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
+
+echo -n 'Round 2: '
+npm run clean > /dev/null
+npm run generate > perf.log
+cat perf.log | grep 'generated in'
