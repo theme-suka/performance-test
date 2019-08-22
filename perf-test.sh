faa5eb03671d828513c4edf0eb560fd0a7c1d618
SukkaW <isukkaw@gmail.com>
2019-08-22 13:43:30 +0800

    Update since hexo pull 3679 is closed

--- perf-test.sh
+++ perf-test.sh
@@ -8,15 +8,14 @@ git clone https://github.com/SukkaLab/hexo-many-posts.git --depth=1 --quiet
 rm -rf .git
 cd ../..
 
+../bin/yq w -i _config.yml theme landscape
+
 echo -n 'Done!'
 
 # Run Clean up
 echo -n '
 * Run clean up ... '
 
-rm -rf themes/suka/source/css/highlight
-rm -rf themes/suka/source/lib/prettify/themes
-rm -rf themes/suka/source/lib/prism
 rm -rf source/assets
 
 echo -n 'Done!
@@ -37,28 +36,17 @@ echo '    - enable: false'
 echo '    - line_number: false'
 echo '    - auto_detect: false'
 echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
 echo '-------------------------------------'
 
 sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
 
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
 ../bin/yq w -i _config.yml highlight.auto_detect false
 ../bin/yq w -i _config.yml highlight.tab_replace false
 
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
 ../bin/yq w -i _config.yml meta_generator false
 ../bin/yq w -i _config.yml external_link false
-
-../bin/yq w -i themes/suka/_config.yml toc.enable false
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
@@ -83,75 +71,17 @@ echo '    - enable: false'
 echo '    - line_number: false'
 echo '    - auto_detect: false'
 echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
 echo '-------------------------------------'
 
 sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
 
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
 ../bin/yq w -i _config.yml highlight.auto_detect false
 ../bin/yq w -i _config.yml highlight.tab_replace false
 
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
 ../bin/yq w -i _config.yml meta_generator false
 ../bin/yq w -i _config.yml external_link false
-
-../bin/yq w -i themes/suka/_config.yml toc.enable false
-
-echo -n 'Round 1: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-echo -n 'Round 2: '
-npm run clean > /dev/null
-npm run generate > perf.log
-cat perf.log | grep 'generated in'
-
-
-npm uninstall hexo --save
-npm install git+https://github.com/curbengh/hexo.git#og-domparser
-
-echo '-------------------------------------'
-echo '               Test C'
-echo ' Use curbengh:og-domparser'
-echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
-echo '-------------------------------------'
-
-sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
-
-../bin/yq w -i _config.yml highlight.enable false
-../bin/yq w -i _config.yml highlight.line_number false
-../bin/yq w -i _config.yml highlight.auto_detect false
-../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
-../bin/yq w -i _config.yml meta_generator false
-../bin/yq w -i _config.yml external_link false
-
-../bin/yq w -i themes/suka/_config.yml toc.enable false
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
@@ -166,7 +96,7 @@ cat perf.log | grep 'generated in'
 npm uninstall hexo --save
 npm install git+https://github.com/sukkaw/hexo.git#lazy-cheerio
 echo '-------------------------------------'
-echo '               Test D'
+echo '               Test C'
 echo ' Lazy cheerio sukkaw:lazy-cheerio'
 echo '-------------------------------------'
 echo ' - fragment_fache: on'
@@ -175,28 +105,17 @@ echo '    - enable: false'
 echo '    - line_number: false'
 echo '    - auto_detect: false'
 echo '    - tab_replace: false'
-echo ' - suka theme prism highlight: off'
-echo ' - suka theme local-search: off'
 echo '-------------------------------------'
 
 sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
-
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
 
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
 ../bin/yq w -i _config.yml highlight.auto_detect false
 ../bin/yq w -i _config.yml highlight.tab_replace false
 
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
 ../bin/yq w -i _config.yml meta_generator false
 ../bin/yq w -i _config.yml external_link false
-
-../bin/yq w -i themes/suka/_config.yml toc.enable false
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
