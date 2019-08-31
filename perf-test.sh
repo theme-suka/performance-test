53d0882dc0020dcb5ba14899ebb5235140bf4bc1
SukkaW <isukkaw@gmail.com>
2019-08-31 21:31:05 +0800

    Add a tets for use regex in meta_generator

--- perf-test.sh
+++ perf-test.sh
@@ -28,17 +28,8 @@ echo '-------------------------------------'
 echo '* Test performance ... '
 echo '-------------------------------------'
 echo '               Test A'
-echo ' Baseline with meta_generator commented out & toc disabled'
+echo ' Baseline: latest hexo with meta_generator disabled'
 echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo '-------------------------------------'
-
-sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
 
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
@@ -58,29 +49,18 @@ npm run clean > /dev/null
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
 
-npm uninstall hexo --save
-npm install git+https://github.com/sukkaw/hexo.git#open-graph-img
 
 echo '-------------------------------------'
 echo '               Test B'
-echo ' Use sukkaw:open-graph-img'
+echo ' latest hexo with meta_generator enabled'
 echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo '-------------------------------------'
-
-sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
 
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
 ../bin/yq w -i _config.yml highlight.auto_detect false
 ../bin/yq w -i _config.yml highlight.tab_replace false
 
-../bin/yq w -i _config.yml meta_generator false
+../bin/yq w -i _config.yml meta_generator true
 ../bin/yq w -i _config.yml external_link false
 
 echo -n 'Round 1: '
@@ -94,27 +74,19 @@ npm run generate > perf.log
 cat perf.log | grep 'generated in'
 
 npm uninstall hexo --save
-npm install git+https://github.com/sukkaw/hexo.git#test-indexof
+npm install git+https://github.com/sukkaw/hexo.git#meta-generator
+
 echo '-------------------------------------'
 echo '               Test C'
-echo ' Lazy cheerio sukkaw:test-indexof'
+echo ' Use sukkaw:meta-generator with meta_generator enabled'
 echo '-------------------------------------'
-echo ' - fragment_fache: on'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo '-------------------------------------'
-
-sed -i "s|filter.register('after_render:html', require('./meta_generator'))|//filter.register('after_render:html', require('./meta_generator'))|g" ./node_modules/hexo/lib/plugins/filter/index.js
 
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
 ../bin/yq w -i _config.yml highlight.auto_detect false
 ../bin/yq w -i _config.yml highlight.tab_replace false
 
-../bin/yq w -i _config.yml meta_generator false
+../bin/yq w -i _config.yml meta_generator true
 ../bin/yq w -i _config.yml external_link false
 
 echo -n 'Round 1: '
