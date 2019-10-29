4d0f5c49980661d74d21d6b4af843a1ff4bb81be
Sukka <isukkaw@gmail.com>
2019-10-29 09:57:12 +0800

    Update perf-test.sh

--- perf-test.sh
+++ perf-test.sh
@@ -19,22 +19,6 @@ rm -rf source/assets
 echo -n 'Done!
 '
 
-npm uninstall hexo --save
-npm install git+https://github.com/hexojs/hexo.git
-
-echo '-------------------------------------'
-echo '* Test performance ... '
-echo '-------------------------------------'
-echo '               Test A'
-echo ' Baseline'
-echo '-------------------------------------'
-echo ' - hexo built in highlight.js'
-echo '    - enable: false'
-echo '    - line_number: false'
-echo '    - auto_detect: false'
-echo '    - tab_replace: false'
-echo '-------------------------------------'
-
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
 ../bin/yq w -i _config.yml highlight.auto_detect false
@@ -42,8 +26,15 @@ echo '-------------------------------------'
 
 ../bin/yq w -i _config.yml meta_generator false
 ../bin/yq w -i _config.yml external_link false
-
 ../bin/yq w -i _config.yml theme landscape
+
+
+echo '-------------------------------------'
+echo '* Test performance ... '
+echo '-------------------------------------'
+echo '               Test A'
+echo ' Baseline'
+echo '-------------------------------------'
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
@@ -55,12 +46,9 @@ npm run clean > /dev/null
 npm run generate > perf.log
 cat perf.log | grep 'generated in'
 
-npm uninstall hexo --save
-npm install git+https://github.com/curbengh/hexo.git#metagen-cheerio
-
 echo '-------------------------------------'
 echo '               Test B'
-echo ' Use https://github.com/sukkaw/hexo.git#lazy-moment'
+echo ' Use https://github.com/sukkaw/hexo-theme-landscape'
 echo '-------------------------------------'
 echo ' - hexo built in highlight.js'
 echo '    - enable: false'
@@ -69,15 +57,9 @@ echo '    - auto_detect: false'
 echo '    - tab_replace: false'
 echo '-------------------------------------'
 
-../bin/yq w -i _config.yml highlight.enable false
-../bin/yq w -i _config.yml highlight.line_number false
-../bin/yq w -i _config.yml highlight.auto_detect false
-../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml meta_generator true
-../bin/yq w -i _config.yml external_link false
-
-../bin/yq w -i _config.yml theme landscape
+rm -rf themes/landscape
+git clone https://github.com/sukkaw/hexo-theme-landscape themes/landscape --depth=1
+rm -rf themes/landscape/.git
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
