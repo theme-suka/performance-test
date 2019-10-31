cddb3245bff33005fc3f212ea91cbedb1bbadfea
Sukka <isukkaw@gmail.com>
2019-10-31 19:37:05 +0800

    Update perf-test.sh

--- perf-test.sh
+++ perf-test.sh
@@ -67,6 +67,7 @@ echo '-------------------------------------'
 
 rm -rf node_modules/warehouse/
 git clone -b "use-rfdc" https://github.com/sukkaw/warehouse node_modules/warehouse
+cd node_modules/warehouse/ && npm i && cd ../..
 
 echo -n 'Round 1: '
 npm run clean > /dev/null
