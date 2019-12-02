141a1bf8e4ee46de4f247fb94f12562b303e9b9e
SukkaW <isukkaw@gmail.com>
2019-12-02 23:03:51 +0800

    Bring up a enhance cache test for yilla

--- perf-test.sh
+++ perf-test.sh
@@ -22,20 +22,11 @@ echo -n 'Done!
 npm uninstall hexo --save
 npm install git+https://github.com/hexojs/hexo.git
 
-../bin/yq w -i themes/suka/_config.yml fragment_cache true
-
 ../bin/yq w -i _config.yml highlight.enable false
 ../bin/yq w -i _config.yml highlight.line_number false
 ../bin/yq w -i _config.yml highlight.auto_detect false
 ../bin/yq w -i _config.yml highlight.tab_replace false
-
-../bin/yq w -i _config.yml suka_theme.prism.enable false
-../bin/yq w -i _config.yml suka_theme.prism.line_number false
-
-../bin/yq w -i _config.yml suka_theme.search.enable false
-
-../bin/yq w -i _config.yml meta_generator false
-../bin/yq w -i _config.yml external_link false
+../bin/yq w -i _config.yml theme yilla
 
 echo '-------------------------------------'
 echo '* Test performance ... '
@@ -66,11 +57,38 @@ cat build.log | grep "Maximum resident set size"
 
 
 npm uninstall hexo --save
-npm install git+https://github.com/sukkaw/hexo.git#htmlparser2-toc
+npm install git+https://github.com/sukkaw/hexo.git#feat_enhance_cache
 
 echo '-------------------------------------'
 echo '               Test B'
-echo ' Use sukkaw:htmlparser2-toc'
+echo ' Use sukkaw:feat_enhance_cache'
+echo '-------------------------------------'
+
+echo -n 'Round 1: '
+npm run clean > /dev/null
+time -v npx --no-install hexo g --debug > build.log
+cat build.log | grep "Hexo version"
+cat build.log | grep "Start processing"
+cat build.log | grep "loaded in"
+cat build.log | grep "generated in"
+cat build.log | grep "Database saved"
+cat build.log | grep "Maximum resident set size"
+
+echo -n 'Round 2: '
+npm run clean > /dev/null
+time -v npx --no-install hexo g --debug > build.log
+cat build.log | grep "Hexo version"
+cat build.log | grep "Start processing"
+cat build.log | grep "loaded in"
+cat build.log | grep "generated in"
+cat build.log | grep "Database saved"
+cat build.log | grep "Maximum resident set size"
+
+../bin/yq w -i _config.yml enhance_cache true
+
+echo '-------------------------------------'
+echo '               Test C'
+echo 'Enable enhance cache'
 echo '-------------------------------------'
 
 echo -n 'Round 1: '
